5038d734052aea83214be360900812956c1327c3
Hassan AlQattan <hassanalqattan@Mac-Studio.local>
2025-10-22 14:12:32 +0300

    Fix: Use only supported properties in theme.ts

--- frontend/theme.ts
+++ frontend/theme.ts
@@ -1,27 +1,20 @@
 import { buildLegacyTheme } from 'sanity'
 
-const props = {
-  '--white': '#ffffff',
-  '--black': '#000000',
-  '--brand': '#000000',
-}
-
 export const customTheme = buildLegacyTheme({
-  '--black': props['--black'],
-  '--white': props['--white'],
-  '--brand': props['--brand'],
+  '--black': '#000000',
+  '--white': '#ffffff',
 
-  '--default-button-color': props['--black'],
-  '--default-button-primary-color': props['--brand'],
+  '--default-button-color': '#000000',
+  '--default-button-primary-color': '#000000',
   '--default-button-success-color': '#43d675',
   '--default-button-warning-color': '#fbd024',
   '--default-button-danger-color': '#f03e2f',
 
-  '--state-info-color': props['--brand'],
+  '--state-info-color': '#000000',
   '--state-success-color': '#43d675',
   '--state-warning-color': '#fbd024',
   '--state-danger-color': '#f03e2f',
 
-  '--main-navigation-color': props['--black'],
-  '--focus-color': props['--black'],
+  '--main-navigation-color': '#000000',
+  '--focus-color': '#000000',
 })
